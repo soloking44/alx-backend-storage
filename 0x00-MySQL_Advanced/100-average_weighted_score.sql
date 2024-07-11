0a8a9ae2c0f2395ca4bc30e84f44eef3d172da08
soloking44 <solop8483@gmail.com>
2024-07-11 06:20:26 -0700

    100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,30 @@
+-- This is an SQL script to create an stored procedure ComputeAverageWeightedScoreForUser
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+  DECLARE tot_weighted_score INT DEFAULT 0;
+  DECLARE tot_weight INT DEFAULT 0;
+
+  SELECT SUM(corrections.score * projects.weight) INTO tot_weighted_score
+  FROM corrections 
+  INNER JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  SELECT SUM(projects.weight) INTO tot_weight FROM corrections
+  INNER JOIN projects ON corrections.project_id = projects.id
+  WHERE corrections.user_id = user_id;
+
+  IF tot_weight = 0 THEN
+    UPDATE users
+    SET users.average_score = 0
+    WHERE users.id = user_id;
+  ELSE
+    UPDATE users
+    SET users.average_score = tot_weighted_score / tot_weight
+    WHERE users.id = user_id;
+    END IF;
+END //
