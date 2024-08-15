c980739db293718ced0cecf10bca09c93caab17b
soloking44 <solop8483@gmail.com>
2024-08-15 03:59:08 -0700

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,15 +12,15 @@ redis_ = redis.Redis()
 def count_requests(method: Callable) -> Callable:
     """ Decortator for counting """
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
+    def wrapper(url):  
         """ Wrapper for decorator """
         redis_.incr(f"count:{url}")
         cached_html = redis_.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
         html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+        redis_.setex(f"cached:{url}", 10, html.encode('utf-8'))  # encode html to bytes
+        return html.decode('utf-8')  # decode html to string
 
     return wrapper
 
