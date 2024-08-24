fe1f3b145fa344d3c4ff96868625cb7ef10a14bb
soloking44 <solop8483@gmail.com>
2024-08-24 12:15:24 -0700

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
-""" Redis Module """
+"""Implementing an expiring web cache and tracker"""
 
 from functools import wraps
 import redis
 import requests
 from typing import Callable
 
-redis_ = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Decorator for counting """
+    """ Decorator for counting how many times a request
+    has been made """
+
     @wraps(method)
-    def wrapper(url):  
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
-        try:
-            html = method(url)
-            redis_.setex(f"cached:{url}", 10, html.encode('utf-8'))
-            return html
-        except requests.RequestException as e:
-            print(f"Error fetching URL {url}: {e}")
-            return None
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
-def get_page_without_cache(url: str) -> str:
-    """ Obtain the HTML content of a  URL without caching """
+@count_requests
+def get_page(url: str) -> str:
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
     req = requests.get(url)
-    req.raise_for_status()  # Raise an exception for bad status codes
     return req.text
-
-
-get_page = count_requests(get_page_without_cache)
